0ff2ba258750978ac32d0362a0ffaf647768a8d1
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-05 18:06:57 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,6 +7,6 @@
 # }
 
 def select_winner(passengers)
-  passengers.select{ |suite, name| suite == "suite_a:" && name[0] == "A"}
-  # result
+  result = passengers.select{ |suite, name| suite == "suite_a:" && name[0] == "A"}
+  result
 end
