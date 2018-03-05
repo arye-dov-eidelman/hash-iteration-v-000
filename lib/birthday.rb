daf85319561985a8f93436a72f7ce971e61b6ba6
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-05 17:49:09 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,9 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each { |name, age| "Happy Birthday #{name}! You are now #{age} years old!"} 
+  birthday_kids.each { |name, age| "Happy Birthday #{name}! You are now #{age} years old!"}
 end
-
-
